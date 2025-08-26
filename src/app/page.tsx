84eceabbe95e54dfcf5c36a54c173a4047c41b35
ranjam <ryanzam2005@gmail.com>
2025-08-26 22:22:37 +0545

    implement basic user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-import { Button } from "@/components/ui/button";
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { authClient } from '@/lib/auth-client';
 
 export default function Home() {
+
+  const { data: session } = authClient.useSession()
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (session) {
+    return <div>
+      <p>Signed in as {session.user?.name}</p>
+      <Button onClick={() => authClient.signOut()}>Sign out</Button>
+    </div>
+  }
+
+  const onSubit = () => {
+    authClient.signUp.email({
+      name,
+      email,
+      password
+    }, {
+      onError: () => alert("Error signing up"),
+      onSuccess: () => alert("Signed up successfully")
+    })
+  }
+
   return (
-    <Button>Click me</Button>
+    <div>
+      <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+      <Button onClick={onSubit}>Sign up</Button>
+    </div>
   );
 }
